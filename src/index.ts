ba1c90e079647711e2b0d1bf7c2644aa730013a7
Jesse Stuart <hi@jessestuart.com>
2019-07-18 12:33:18 -0400

    [commit message skipped]

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,14 @@
-import axios from 'axios'
-import _ from 'lodash'
-import fp from 'lodash/fp'
+import { popuplateDynamoWithQueryResults } from './aws/populate-dynamodb'
+import { queryReposForUser } from './services/docker-hub'
 
-const NUM_REPOS_TO_ANALYZE = 30
-const QUERY_PARAMS = {
-  params: {
-    page: 1,
-    page_size: NUM_REPOS_TO_ANALYZE,
-  },
-}
-
-interface QueryOptions {
-  username: string
-}
-
-interface RepoFields {
-  description: string
-  last_updated: string
-  name: string
-  pull_count: number
-  star_count: number
-}
-
-interface DHStatsReponse {
-  topRepos: RepoFields[]
-  totalPulls: number
-}
-
-export const queryRepos = async ({
-  username,
-}: QueryOptions): Promise<DHStatsReponse> => {
-  const repos = await axios.get(
-    `https://hub.docker.com/v2/repositories/${username}`,
-    QUERY_PARAMS,
-  )
-  const topRepos: RepoFields[] = _.flow(
-    fp.get('data.results'),
-    fp.map(
-      fp.pick([
-        'description',
-        'last_updated',
-        'name',
-        'pull_count',
-        'star_count',
-      ]),
-    ),
-  )(repos) as RepoFields[]
-  const totalPulls: number = _.sumBy(topRepos, 'pull_count')
-
-  return { topRepos, totalPulls }
-}
-
-// queryRepos({ username: USERNAME })
-//   .then(({ topRepos, totalPulls }) => {
-//     console.log({ topRepos, totalPulls })
-//   })
-//   .catch(console.error)
+queryReposForUser({ username: 'jessestuart' })
+  .then(async ({ topRepos, totalPulls }) => {
+    const results = await popuplateDynamoWithQueryResults({
+      topRepos,
+    })
+    console.log({ results })
+    return { topRepos, totalPulls }
+  })
+  .catch(err => {
+    throw new Error(err)
+  })

6dde4de52c1ea39a99e07c1f5d10f9746897c1e8
Jesse Stuart <hi@jessestuart.com>
2019-07-17 23:47:35 -0400

    [wip]
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- src/aws/populate-dynamodb.ts
+++ src/aws/populate-dynamodb.ts
@@ -5,15 +5,17 @@ import { queryRepos } from '../index'
 
 AWS.config.update({ region: 'us-east-1' })
 
+// @ts-ignore
 const Dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 
 queryRepos({ username: 'jessestuart' })
   .then(result => {
     result.topRepos.forEach(repo => {
+      // @ts-ignore
       const { description, last_updated, name, pull_count, star_count } = repo
       const params = {
         Item: {
-          LAST_UPDATED: last_updated,
+          LAST_UPDATED: new Date(last_updated).getTime(),
           NAME: name,
           PULL_COUNT: _.toNumber(pull_count),
           STAR_COUNT: _.toNumber(star_count),
@@ -24,6 +26,26 @@ queryRepos({ username: 'jessestuart' })
         _.set(params, 'Item.DESCRIPTION', description)
       }
 
+      // const updateParams = {
+      //   ExpressionAttributeValues: {
+      //     ':last_updated': last_updated,
+      //     // ':pull_count': pull_count,
+      //     ':star_count': star_count,
+      //   },
+      //   Key: { NAME: name, PULL_COUNT: pull_count },
+      //   ReturnValues: 'UPDATED_NEW',
+      //   TableName: 'jds-docker-hub',
+      //   UpdateExpression: `
+      //     set STAR_COUNT=:star_count, LAST_UPDATED=:last_updated
+      //   `,
+      // }
+      // Dynamo.update(updateParams, (err, data) => {
+      //   if (err) {
+      //     console.error(err)
+      //   } else {
+      //     console.log('Success: ', { data })
+      //   }
+      // })
       Dynamo.put(params, (err, data) => {
         if (err) {
           console.error('Error', err)

4add0c0618a905f1c19b45ebce4e33bf15df7ce1
Jesse Stuart <hi@jessestuart.com>
2019-07-19 01:36:25 -0400

    [commit message skipped]

--- src/aws/populate-dynamodb.ts
+++ src/aws/populate-dynamodb.ts
@@ -1,10 +1,10 @@
 import AWS from 'aws-sdk'
+import _ from 'lodash'
 
 import { queryRepos } from '../index'
 
 AWS.config.update({ region: 'us-east-1' })
 
-// @ts-ignore
 const Dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 
 // @ts-ignore
@@ -30,34 +30,26 @@ const Dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 queryRepos({ username: 'jessestuart' })
   .then(result => {
     result.topRepos.forEach(repo => {
-      // @ts-ignore
       const { description, last_updated, name, pull_count, star_count } = repo
-      // const params = {
-      //   Item: {
-      //     LAST_UPDATED: new Date(last_updated).getTime(),
-      //     NAME: name,
-      //     PULL_COUNT: _.toNumber(pull_count),
-      //     STAR_COUNT: _.toNumber(star_count),
-      //   },
-      //   TableName: 'jds-docker-hub',
-      // }
-      // if (description) {
-      //   _.set(params, 'Item.DESCRIPTION', description)
-      // }
 
       const updateExpression = `
         set
           STAR_COUNT=:star_count,
           PULL_COUNT=:pull_count,
           LAST_UPDATED=:last_updated
-          ${description != null ? ', DESCRIPTION=:description' : null}
+          ${description != null ? ', DESCRIPTION=:description' : ''}
       `
+
+      const ExpressionAttributeValues = {
+        ':last_updated': last_updated,
+        ':pull_count': pull_count,
+        ':star_count': star_count,
+      }
+      if (!_.isEmpty(description)) {
+        _.set(ExpressionAttributeValues, ':description', description)
+      }
       const updateParams = {
-        ExpressionAttributeValues: {
-          ':last_updated': last_updated,
-          ':pull_count': pull_count,
-          ':star_count': star_count,
-        },
+        ExpressionAttributeValues,
         Key: { NAME: name },
         ReturnValues: 'UPDATED_NEW',
         TableName: 'jds-docker-hub',
@@ -70,29 +62,6 @@ queryRepos({ username: 'jessestuart' })
           console.log('Success: ', { data })
         }
       })
-      // Dynamo.put(params, (err, data) => {
-      //   if (err) {
-      //     console.error('Error', err)
-      //   } else {
-      //     console.log('Success', data)
-      //   }
-      // })
     })
-    // async.map(result.topRepos, processRepo, (err, queryRepoResults) => {
-    //   if (err != null) {
-    //     return Promise.reject(err)
-    //   }
-    //   return Promise.resolve(queryRepoResults)
-    // })
   })
   .catch(console.error)
-
-// queryRepos({ username: 'jessestuart' })
-//   .then(result => {
-//     result.topRepos.forEach(async repo => {
-//       const processRepoResults = await processRepo(repo)
-//       console.log({ processRepoResults })
-//       // results.then(console.log).catch(console.error)
-//     })
-//   })
-//   .catch(console.error)

f572559c00292b43758e173966d8c6ae49971e0c
Jesse Stuart <hi@jessestuart.com>
2019-07-19 05:43:46 -0400

    feat(docker): Build Docker image in CI.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

---
+++ .circleci/load_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo '
+export GITHUB_REPO=jessestuart/ts-docker-hub
+export IMAGE=ts-docker-hub
+export REGISTRY=jessestuart
+export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export DIR=`pwd`
+export QEMU_VERSION="v4.0.0"
+' >>$BASH_ENV
+
+. $BASH_ENV

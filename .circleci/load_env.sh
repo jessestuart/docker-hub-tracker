10527a86ade9bbdf59f2c85fa7a4a2cb4044eb7e
Jesse Stuart <hi@jessestuart.com>
2019-07-19 09:41:02 -0400

    fix(misc): Update repo name.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo '
-export GITHUB_REPO=jessestuart/ts-docker-hub
-export IMAGE=ts-docker-hub
+export GITHUB_REPO=jessestuart/docker-hub-tracker
+export IMAGE=docker-hub-tracker
 export REGISTRY=jessestuart
-export VERSION=$(curl -s https://api.github.com/repos/jessestuart/ts-docker-hub/releases/latest | jq -r ".tag_name")
+export VERSION=$(curl -s https://api.github.com/repos/jessestuart/docker-hub-tracker/releases/latest | jq -r ".tag_name")
 export DIR=`pwd`
 export QEMU_VERSION="v4.0.0"
 ' >>$BASH_ENV

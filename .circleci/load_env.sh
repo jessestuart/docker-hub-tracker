bf3b34159310f0b3e3b2d74516b33ec668afb269
Jesse Stuart <hi@jessestuart.com>
2019-07-19 09:15:14 -0400

    fix(github): Trigger GH release.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -4,7 +4,7 @@ echo '
 export GITHUB_REPO=jessestuart/ts-docker-hub
 export IMAGE=ts-docker-hub
 export REGISTRY=jessestuart
-export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export VERSION=$(curl -s https://api.github.com/repos/jessestuart/ts-docker-hub/releases/latest | jq -r ".tag_name")
 export DIR=`pwd`
 export QEMU_VERSION="v4.0.0"
 ' >>$BASH_ENV
